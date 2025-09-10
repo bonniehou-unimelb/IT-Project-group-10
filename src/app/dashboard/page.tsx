f088d74c0094f4b887c5d6e5dfced3cb7f625201
mdln-wgnr <madeline.wagner@student.unimelb.edu.au>
2025-09-10 22:05:19 +1000

    started working on dashboard layout

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+export default function Dashboard() {
+    return (
+        <div className="min-h-screen bg-gray-100">
+            <div className="min-h-13 bg-white p-4 box-border">
+                <h1 className="text-gray-500"> Dashboard </h1>
+            </div>
+            <div className="min-h-screen max-w-50 bg-blue-900">
+                
+            </div>
+            
+        </div>
+    );
+}

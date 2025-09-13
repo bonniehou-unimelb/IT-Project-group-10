2dd5c958c729c8a6cafd34ec4fad7c20ff10e11e
mdln-wgnr <madeline.wagner@student.unimelb.edu.au>
2025-09-13 18:47:30 +1000

    added drop down menu to top right

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,11 @@
 "use client";
 import { useState } from "react";
-import Table from 'react';
+import Image from "next/image";
+
+{/* Dependencies required for drop down menu */}
+import { Menu, MenuItem, MenuButton } from '@szhsin/react-menu';
+import '@szhsin/react-menu/dist/index.css';
+import '@szhsin/react-menu/dist/transitions/zoom.css';
 
 export default function Dashboard() {
     const [search, setSearch] = useState("");
@@ -8,7 +13,7 @@ export default function Dashboard() {
     return (
         <div className="max-h-screen bg-gray-100">
             <h1 className="text-3xl text-black font-bold translate-x-52 translate-y-15"> Welcome Back *username*!</h1>
-            
+
             {/* Search Bar */}
             <div className="translate-x-52 translate-y-17">
               <div>
@@ -18,7 +23,7 @@ export default function Dashboard() {
                         type="text"
                         value={search}
                         onChange={(t) => setSearch(t.target.value)}
-                        className="pl-1 pr-1 py-3 min-w-250 bg-gray-200 border-gray-600 rounded-xl text-gray-900 hover:bg-gray-300"
+                        className="pl-1 pr-1 py-3 min-w-240 bg-gray-200 border-gray-600 rounded-xl text-gray-900 hover:bg-gray-300"
                         placeholder="🔍 Search"
                         required
                     />
@@ -33,12 +38,26 @@ export default function Dashboard() {
             </div>
 
             {/* Top Panel */}
-            <div className="min-h-13 bg-white p-4 translate-x-50 -translate-y-22">
-                <p className="text-gray-500"> Dashboard  </p>
+            <div className="min-h-13 max-h-13 bg-white p-4 translate-x-50 -translate-y-22">
+                <p className="text-gray-500"> Dashboard </p>
+                <div className="float-right -translate-x-60 -translate-y-7">
+                    <Image
+                        src="icons/profile-filler.svg"
+                        alt="profile picture"
+                        width={30}
+                        height={30}
+                    />
+                    <div className="translate-x-8 -translate-y-6.5">
+                        <Menu menuButton={<MenuButton>*username* ▿</MenuButton>} transition>
+                            {/* TODO: Link this back to the log in page */}
+                            <MenuItem>Log Out</MenuItem>
+                        </Menu>
+                    </div>
+                </div>
             </div>
 
             {/* Side Menu */}
-            <div className="min-h-screen max-w-50 bg-blue-900 -translate-y-35">
+            <div className="min-h-screen max-w-50 bg-blue-900 -translate-y-34">
                 <p className="font-bold text-xl text-white translate-x-6 translate-y-4"> ⚙️ Dashboard </p>
             </div>
         </div>

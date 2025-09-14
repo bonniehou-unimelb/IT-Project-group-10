9522923252551ca9b746dc0eaa569fde2cd9ee27
mdln-wgnr <madeline.wagner@student.unimelb.edu.au>
2025-09-14 15:59:29 +1000

    added dummy table. Added button to create new template

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -29,16 +29,59 @@ export default function Dashboard() {
                     />
                     <button
                         type="submit"
-                        className="translate-x-2 min-h-10 min-w-20 py-3 rounded-xl bg-blue-700 text-white font-semibold hover:bg-blue-800 disabled:opacity-50 shadow-lg transition"
-                    > 
+                        className="translate-x-2 min-h-10 min-w-20 py-3 rounded-xl bg-blue-700 text-white font-semibold hover:bg-blue-800 disabled:opacity-50 shadow-lg transition"> 
                         Submit
                     </button>
                 </form>
               </div>
             </div>
 
+            {/* Create new template button */}
+            {/* TODO: Link to table creation page later */}
+            <button
+                type="button"
+                className="translate-x-52 translate-y-20 text-white py-1 px-2 rounded-lg bg-blue-700 font-semibold hover:bg-blue-800 shadow-lg">
+                + Create New Template
+            </button>
+
+            {/* Navigation Table */}
+            {/* TODO: Fix so it reads this data from the sql. Just sample data in there at the moment*/}
+            <div className="translate-x-52 translate-y-25">
+                <table className="border-collapse border border-gray-300 table-auto">
+                    <thead>
+                        <tr>
+                            <th className="column-padding border text-left"> Template Name </th>
+                            <th className="column-padding border text-left"> Assessment </th>
+                            <th className="column-padding border text-left"> Subject </th>
+                            <th className="column-padding border text-left"> Creator </th>
+                            <th className="column-padding border text-left"> Actions </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td className="column-padding border text-left"> Template 1 </td>
+                            <td className="column-padding border text-left"> Assignment 1 </td>
+                            <td className="column-padding border text-left"> COMP30022 </td>
+                            <td className="column-padding border text-left"> John Smith </td>
+                            <td className="column-padding border text-left"> 
+                                <button 
+                                    type="submit"
+                                    className="rounded-xl border border-blue-900 font-semibold px-2 hover:bg-gray-200">
+                                    Preview
+                                </button>
+                                <button
+                                    type="submit"
+                                    className="translate-x-1 rounded-xl border border-blue-900 font-semibold px-2 hover:bg-gray-200">
+                                    Duplicate
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
             {/* Top Panel */}
-            <div className="min-h-13 max-h-13 bg-white p-4 translate-x-50 -translate-y-22">
+            <div className="min-h-13 max-h-13 bg-white p-4 translate-x-50 -translate-y-45">
                 <p className="text-gray-500"> Dashboard </p>
                 <div className="float-right -translate-x-60 -translate-y-7">
                     <Image
@@ -57,7 +100,7 @@ export default function Dashboard() {
             </div>
 
             {/* Side Menu */}
-            <div className="min-h-screen max-w-50 bg-blue-900 -translate-y-34">
+            <div className="min-h-screen max-w-50 bg-blue-900 -translate-y-57">
                 <p className="font-bold text-xl text-white translate-x-6 translate-y-4"> ⚙️ Dashboard </p>
             </div>
         </div>

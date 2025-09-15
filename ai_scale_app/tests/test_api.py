cdd4c8afd66e69785b04ea03572e33ade2e0bbaf
Bonnie <bojueh@student.unimelb.edu.au>
2025-09-15 16:09:20 +1000

    test cases

--- ai_scale_app/tests/test_api.py
+++ ai_scale_app/tests/test_api.py
@@ -7,9 +7,9 @@ User = get_user_model()
 
 class LoginApiTests(TestCase):
     def setUp(self):
-        # Create a test user
-        self.username = "testuser"
-        self.password = "testpass123"
+        # create test user
+        self.username = "dummyuser"
+        self.password = "dummypassword"
         self.user = User.objects.create_user(
             username=self.username,
             password=self.password,
@@ -18,7 +18,7 @@ class LoginApiTests(TestCase):
 
     def test_login_success(self):
         response = self.client.post(
-            "/auth/login/",  # or use reverse("user_login") if named
+            "/auth/login/",  
             {"username": self.username, "password": self.password},
             content_type="application/json"
         )
@@ -37,7 +37,7 @@ class LoginApiTests(TestCase):
     def test_login_missing_fields(self):
         response = self.client.post(
             "/auth/login/",
-            {"username": self.username},  # no password
+            {"username": self.username},  # missing password
             content_type="application/json"
         )
         self.assertEqual(response.status_code, 400)

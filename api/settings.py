e38dd4048e593cb1dee0ee947a25013b335dd4ad
chenhzhang2 <winston.zhang2@student.unimelb.edu.au>
2025-10-26 02:36:49 +1100

    added changes

--- api/settings.py
+++ api/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "corsheaders",
+    "storages",
 ]
 
 MIDDLEWARE = [
@@ -92,7 +93,9 @@ DATABASES = {
 AUTH_USER_MODEL = "ai_scale_app.User"
 
 AUTH_PASSWORD_VALIDATORS = [
-    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
+    },
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
@@ -100,8 +103,15 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # ========= Static files required for Render =========
-STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"  # required
+STATIC_URL = f'https://{os.getenv("AWS_STORAGE_BUCKET_NAME")}.s3.amazonaws.com/'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# AWS credentials
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = 'ap-northeast-1'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
 
 
 LANGUAGE_CODE = "en-us"

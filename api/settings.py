0b5b0c61bd59aa811ba4f110c77ba4d8905e81e3
chenhzhang2 <winston.zhang2@student.unimelb.edu.au>
2025-11-03 18:45:14 +1100

    changed cookie settings

--- api/settings.py
+++ api/settings.py
@@ -130,4 +130,3 @@ SESSION_COOKIE_SECURE = True  # required for HTTPS
 SESSION_COOKIE_SAMESITE = "None"  # allow cross-site cookies
 CSRF_COOKIE_SAMESITE = "None"
 CSRF_COOKIE_SECURE = True
-CSRF_COOKIE_HTTPONLY = False  # frontend needs to read the cookie
